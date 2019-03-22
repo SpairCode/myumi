2280ef1230feb9aa842b49cf9800dcf7713bbc79
SpairFan <spairfan@126.com>
2019-03-22 11:41:54 +0800

    edit index page layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,91 @@
 import styles from './index.css';
-import Header from '../pages/header/index'
-import Footers from '../pages/footer/footer'
 import withRouter from 'umi/withRouter'
 import { connect } from 'dva'
+import { Layout, Menu, Icon, Badge, Avatar, Dropdown } from 'antd'
+import "antd/dist/antd.css"
 
 function BasicLayout(props) {
   if (props.location.pathname === '/login/login' || props.location.pathname === '/404') {
     return <div className={`${styles.w100} ${styles.h100} `}>{ props.children }</div>
   } else {
+    const menulist = (
+      <Menu>
+        <Menu.Item>
+          <Icon type="user" /> 个人中心
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item>
+        <Icon type="setting" /> 设置
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item>
+          <Icon type="logout" /> 退出登录
+        </Menu.Item>
+      </Menu>
+    )
+    const { Header, Content, Footer, Sider } = Layout
     return (
       <div className={styles.normal}>
-        <Header></Header>
         <div className={styles.content}>
-        { props.children }
+        <div className={styles.normal}>
+          <Layout className={styles.antWH}>
+              <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => { console.log(broken); }}
+                onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
+                theme="light"
+              >
+                <div className="logo" />
+                <Menu theme="light" mode="inline" defaultSelectedKeys={['4']}>
+                  <Menu.Item key="1">
+                    <Icon type="user" />
+                    <span className="nav-text">nav 1</span>
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                    <Icon type="video-camera" />
+                    <span className="nav-text">nav 2</span>
+                  </Menu.Item>
+                  <Menu.Item key="3">
+                    <Icon type="upload" />
+                    <span className="nav-text">nav 3</span>
+                  </Menu.Item>
+                  <Menu.Item key="4">
+                    <Icon type="user" />
+                    <span className="nav-text">nav 4</span>
+                  </Menu.Item>
+                </Menu>
+              </Sider>
+              <Layout style={{ backgroundColor: '#ebeef6' }}>
+                {/* Gobal Header */}
+                <Header style={{ background: '#fff', padding: 0 }} >
+                  <div className={styles.header} style={{ float: 'right', }}>
+                    <Badge dot>
+                      <Icon style={{ fontSize: 22 }} type="bell"></Icon>
+                    </Badge>
+                    <Dropdown overlay={menulist}>
+                      <span>
+                        <Avatar src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2637329911,3521077389&fm=58&bpow=649&bpoh=808" />
+                        <span style={{ margin: '0px 5px' }}> Fancy </span>
+                        <Icon type="caret-down"/>
+                      </span>
+                    </Dropdown>
+                  </div>
+                </Header>
+                {/* Gobal Content */}
+                <Content style={{ margin: '24px 16px 0', height: '100%' }}>
+                  <div style={{ padding: 24, background: '#fff', height: '100%' }}>
+                    { props.children }
+                  </div>
+                </Content>
+                {/* Gobal Footer */}
+                <Footer style={{ textAlign: 'center', backgroundColor: '#ebeef6' }}>
+                  AntD & UmiJS & Dva
+                </Footer>
+              </Layout>
+            </Layout>
+          </div>
         </div>
-        <Footers></Footers>
       </div>
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,10 @@
 import styles from './index.css'
-// import Link from 'umi/link'
-// import router from 'umi/router'
-// import { Button } from 'antd'
-import { Row, Col } from 'antd'
 import "antd/dist/antd.css"
-import { Carousel } from 'antd'
 
 export default function(props) {
   return (
     <div className={styles.normal}>
-      {/* 轮播图 */}
-      <div className={styles.banner}>
-        <Carousel vertical autoplay="true" effect="fade">
-          <div className={styles.banner}>
-            <h1> Hello Fan </h1>
-          </div>
-          <div className={styles.banner}>
-            <h1> Hello Fans </h1>
-          </div>
-          <div className={styles.banner}>
-            <h1> Hello Roy </h1>
-          </div>
-          <div className={styles.banner}>
-            <h1> <a href="http://www.uptechina.com/UniWebView.unitypackage" class="download">  Quick DownLoad </a> </h1>
-          </div>
-        </Carousel>
-      </div>
-      <div className={styles.list}>
-        <Row type="flex" justify="space-around" align="middle">
-          <Col className={styles.orange} span={6}></Col>
-          <Col className={styles.purple} span={6}></Col>
-          <Col className={styles.blue} span={6}></Col>
-        </Row>
-      </div>
+      content
     </div>
   )
 }
-
-// umi 路由跳转测试
-// function Test () {
-//   router.push('/404')
-// }
